7f027bec6de4ee70aaf57aa7578abab6b5721632
Nolami <94444366+Nolami@users.noreply.github.com>
2024-08-02 16:57:52 +0300

    Why did I do this?

--- actual.js
+++ actual.js
@@ -45,3 +45,47 @@ function div() {
 
     document.getElementById('resdiv').innerText = `Answer: ${result}`
 }
+
+function exp() {
+
+    const num = parseFloat(document.getElementById('numex').value)
+    const exp = parseInt(document.getElementById('exp').value)
+
+    let result = 1
+
+    if (exp === 0) {
+        result = 1
+    } else if (exp > 0) {
+        for (let i = 0; i < exp; i++) {
+            result *= num
+        }
+    } else {
+        for (let i = 0; i < Math.abs(exp); i++) {
+            result /= num
+        }
+    }
+
+    
+
+    document.getElementById('resexp').innerText = `Answer: ${result}`
+}
+function root() {
+    const num = parseFloat(document.getElementById('numrt').value)
+    const root = parseFloat(document.getElementById('rt').value)
+
+    if (root === 0) {
+        document.getElementById('resrt').innerText = `undefined`
+        return;
+    }
+
+    let result;
+
+    if (num < 0 && root % 2 === 0) {
+        document.getElementById('resrt').innertext = `Negative root can't have a even number`
+        return;
+    }
+
+    result = Math.pow(num, 1 / root);
+
+    document.getElementById('resrt').innerText = `${result}`;
+}
